4ab414386a963e1f9d3f83681926a80c62b40e0c
VictorRobin1097@gmail.com <VictorRobin1097@gmail.com>
2025-12-27 17:34:33 +0800

    添加FilteredStockItem 响应注释

--- src/api_models/stock.rs
+++ src/api_models/stock.rs
@@ -4,11 +4,11 @@ use serde::Serialize;
 pub struct FilteredStockItem {
     pub f57: String,   // 代码
     pub f58: String,   // 名称
-    pub f43: Option<f64>,
-    pub f170: Option<f64>,
-    pub f50: Option<f64>,
-    pub f168: Option<f64>,
-    pub f191: Option<f64>,
-    pub f137: Option<f64>,
+    pub f43: Option<f64>, // 最新价
+    pub f170: Option<f64>, // 涨跌幅
+    pub f50: Option<f64>, // 量比
+    pub f168: Option<f64>, // 换手率
+    pub f191: Option<f64>, // 委比
+    pub f137: Option<f64>, // 主力净流入
 }
 
