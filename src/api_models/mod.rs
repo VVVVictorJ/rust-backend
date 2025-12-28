e7f164c4d8331d6714fa517e7f93bc43f64c8117
VictorRobin1097@gmail.com <VictorRobin1097@gmail.com>
2025-12-28 14:38:13 +0800

    新增kline_import模块及相关API模型

--- src/api_models/mod.rs
+++ src/api_models/mod.rs
@@ -4,10 +4,12 @@ pub mod stock_snapshot;
 pub mod profit_analysis;
 pub mod stock_request_stock;
 pub mod daily_kline;
+pub mod kline_import;
 
 pub use stock_request::{StockRequestResponse, CreateStockRequest};
 pub use stock_snapshot::{StockSnapshotResponse, CreateStockSnapshot, TodayStockCodesResponse};
 pub use profit_analysis::{ProfitAnalysisResponse, CreateProfitAnalysis};
 pub use stock_request_stock::{CreateStockRequestStock, StockRequestStockResponse};
 pub use daily_kline::{CreateDailyKline, DailyKlineResponse};
+pub use kline_import::{ImportKlineRequest, ImportKlineResponse};
 
